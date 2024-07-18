fe82eacf0efe3a9d2351c49a48bd66fcb92c12c5
Soumi033 <sarkarsoumi621@gmail.com>
2024-07-18 14:29:17 +0530

    upload page

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -13,8 +13,9 @@ import 'react-toastify/dist/ReactToastify.css';
 import ReportPage from './pages/reportPage/reportPage';
 import ProfilePage from './pages/profilePage/profilePage';
 import SchedulePage from './pages/schedulePage/schedulePage';
-import SplashScreen from './pages/splashscreen/SplashScreen';
+import SplashScreen from './pages/splashScreenpage/SplashScreen';
 import Level from './pages/level/Level';
+import FileUpload from './pages/uploadSensorData/UploadFile';
 
 
 function App() {
@@ -34,6 +35,7 @@ function App() {
         <Route path='*' element={<NotFoundPage />}/>
         <Route path='/splashscreen' element={<SplashScreen />}/>
         <Route path='/level' element={<Level/>}/>
+        <Route path='/uploadfile' element={<FileUpload/>}/>
         
       </Routes>
     </BrowserRouter>
