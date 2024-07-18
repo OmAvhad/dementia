6b4930915833d08ac4ed6f22392587c77503db55
adi1999 <adityadhookia@gmail.com>
2024-07-18 13:58:12 +0530

    dummy code

--- server/routes/routes.py
+++ server/routes/routes.py
@@ -4,28 +4,85 @@ from flask import Blueprint, request
 import google.generativeai as genai
 import os
 from run import gemeni_api_key
+from run import faiss_index
+from langchain.chains import RetrievalQA
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_core.prompts import PromptTemplate
+from langchain_google_vertexai import VertexAIEmbeddings, VertexAI
+from dotenv import load_dotenv
+import os
+import google.generativeai as genai
+from run import app
+
 
 genai.configure(api_key=gemeni_api_key)
 model = genai.GenerativeModel('gemini-1.5-flash')
 
 main_bp = Blueprint('main', __name__)
 
-@main_bp.route('/')
-def index():
-    return json.dumps({"message": "Hello, World!"})	
 
-@main_bp.route('/query', methods=['GET'])
-def query():
-    # get query
-    query = request.args.get('query')
+
+def get_response_llm(query):
+    prompt_template = """
+    Human: Answer the question as detailed as possible from the provided context, make sure to provide all the details, humanize the response. 
+    You can provide links as well. Use you external knowledge to make it effective. These questions are for dementia patients, If you don't know the answer,use your knowledge of alzheimer's and 
+    dementia, and also from Dementia UK website.\n
     
-    prompt = "Answer like a professional and caring admiral nurse to the following question: " + query
+    Context:\n {context}?\n
+    Question: {question}
     
-    # fetch response
-    response = model.generate_content(prompt)
+    Assistant:
+    """
+    prompt = PromptTemplate(template=prompt_template, input_variables=['context', 'question'])
 
-    # extract the generated content from the response
-    generated_content = response.text
+    qa = RetrievalQA.from_chain_type(
+        llm=model,
+        retriever=faiss_index.as_retriever(
+            search_type="similarity", search_kwargs={"k": 3}
+        ),
+        chain_type="stuff",
+        return_source_documents=True,
+        chain_type_kwargs={"prompt": prompt}
 
-    # return the generated content as the response
-    return json.dumps({"response": generated_content})
+    )
+    answer = qa({"query": query})
+
+    formatted_answer = answer['result'].split('\n\n')
+    source_values = [doc.metadata.get('source') for doc in answer['source_documents']]
+
+    return formatted_answer, source_values 
+
+# @app.route('/')
+# def index():
+#     return json.dumps({"message": "Hello, World!"})	
+
+# @app.route('/query', methods=['GET'])
+# def query():
+#     # get query
+#     query = request.args.get('query')
+    
+#     prompt = "Answer like a professional and caring admiral nurse to the following question: " + query
+    
+#     # fetch response
+#     response = model.generate_content(prompt)
+
+#     # extract the generated content from the response
+#     generated_content = response.text
+
+#     # return the generated content as the response
+#     return json.dumps({"response": generated_content})
+
+
+# @app.route('/personalised-query', methods=['GET'])
+# def query():
+#     # get query
+#     query = request.args.get('query')
+    
+#     # fetch response
+#     response, links = get_response_llm(query)
+
+#     # extract the generated content from the response
+   
+
+#     # return the generated content as the response
+#     return json.dumps({"response": response, "links": links})
--- server/run.py
+++ server/run.py
@@ -4,11 +4,37 @@
 # from llama_index.core.memory import ChatMemoryBuffer
 # from llama_utils.index import get_index
 # from llama_utils.settings import update_settings
+from langchain_community.vectorstores import FAISS
+from langchain_huggingface import HuggingFaceEmbeddings
 from dotenv import load_dotenv
 import os
+from flask_cors import CORS
+from flask import Flask, request, jsonify
+import json
+from flask import Blueprint, request
+# from run import chat_engine
+import google.generativeai as genai
+import os
+from langchain.chains import RetrievalQA
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_core.prompts import PromptTemplate
+from langchain_google_vertexai import VertexAIEmbeddings, VertexAI
+from dotenv import load_dotenv
+import os
+import google.generativeai as genai
+import vertexai
+import google.auth
+
+# Setting up credentials
+credentials, _ = google.auth.default()
+
+
 load_dotenv()
 
 gemeni_api_key = os.getenv('GOOGLE_API_KEY')
+genai.configure(api_key=gemeni_api_key)
+vertexai.init(project="gen-lang-client-0064345111")
+model = genai.GenerativeModel('gemini-1.5-flash')
 
 # get lama index
 # index = get_index()
@@ -31,10 +57,113 @@ gemeni_api_key = os.getenv('GOOGLE_API_KEY')
 #     verbose=True
 # )
 
-def main():
-    from app import create_app
-    app = create_app()
-    app.run(debug=True, port=5000)
+
+#Aditya-Code
+def get_embeddings():
+
+    modelPath = "Alibaba-NLP/gte-large-en-v1.5"
+
+    model_kwargs = {'device':'cpu', 'trust_remote_code': True}
+
+    encode_kwargs = {'normalize_embeddings': False}
+
+    embeddings = HuggingFaceEmbeddings(
+        model_name=modelPath,     
+        model_kwargs=model_kwargs, 
+        encode_kwargs=encode_kwargs # Pass the encoding options
+        )
+    return embeddings
+
+def get_llm(model_name):
+    llm = VertexAI(model_name=model_name)
+    return llm
+
+def get_response_llm( query):
+    prompt_template = """
+    Human: Answer the question as detailed as possible from the provided context, make sure to provide all the details, humanize the response. 
+    You can provide links as well. Use you external knowledge to make it effective. These questions are for dementia patients, If you don't know the answer,use your knowledge of alzheimer's and 
+    dementia, and also from Dementia UK website.\n
+    
+    Context:\n {context}?\n
+    Question: {question}
+    
+    Assistant:
+    """
+    prompt = PromptTemplate(template=prompt_template, input_variables=['context', 'question'])
+    llm = get_llm(model_name="gemini-1.5-flash")
+
+    qa = RetrievalQA.from_chain_type(
+        llm=llm,
+        retriever=faiss_index.as_retriever(
+            search_type="similarity", search_kwargs={"k": 3}
+        ),
+        chain_type="stuff",
+        return_source_documents=True,
+        chain_type_kwargs={"prompt": prompt}
+
+    )
+    answer = qa({"query": query})
+
+    formatted_answer = answer['result'].split('\n\n')
+    source_values = [doc.metadata.get('source') for doc in answer['source_documents']]
+
+    return formatted_answer, source_values 
+
+embeddings = get_embeddings()
+
+faiss_index = FAISS.load_local("faiss_index", embeddings, allow_dangerous_deserialization=True)
+
+
+app = Flask(__name__)
+CORS(app)
+
+
+
+
+@app.route('/')
+def index():
+    return json.dumps({"message": "Hello, World!"})	
+
+@app.route('/query', methods=['GET'])
+def query():
+    # get query
+    query = request.args.get('query')
+    
+    prompt = "Answer like a professional and caring admiral nurse to the following question: " + query
+    
+    # fetch response
+    response = model.generate_content(prompt)
+
+    # extract the generated content from the response
+    generated_content = response.text
+
+    # return the generated content as the response
+    return json.dumps({"response": generated_content})
+
+
+@app.route('/personalised-query', methods=['GET'])
+def peronalisedquery():
+    # get query
+    query = request.args.get('query')
+    
+    # fetch response
+    response, links = get_response_llm(query)
+
+    # extract the generated content from the response
+   
+
+    # return the generated content as the response
+    return json.dumps({"response": response, "links": links})
 
 if __name__ == '__main__':
-    main()
+    app.run(debug=False)
+    
+
+# def main():
+#     from app import create_app
+#     app = create_app()
+#     CORS(app)
+#     app.run(debug=True, port=5000)
+
+# if __name__ == '__main__':
+#     main()
